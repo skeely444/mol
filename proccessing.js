fda2053aae91ce4cdffd0d0a750bba48eb153ed7
skeely444 <mauro.camerlinckx@telenet.be>
2025-05-04 19:45:03 +0200

    worked a lot on the script

--- proccessing.js
+++ proccessing.js
@@ -2,7 +2,7 @@ let player1Name, player2Name, player3Name, player4Name, player5Name, deMolName, 
 function start() {
     const body = document.querySelector("body")
     const player1InputField = document.createElement("input")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     player1InputField.id = "player1Input"
     body.appendChild(player1InputField)
@@ -14,7 +14,7 @@ function start() {
 function player2() {
     player1Name = document.getElementById("player1Input").value
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     console.log(`Welkom ${player1Name}`)
     const removedInput = document.getElementById("player1Input")
@@ -29,7 +29,7 @@ function player2() {
 function player3() {
     player2Name = document.getElementById("player2Input").value
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     console.log(`Welkom ${player2Name}`)
     const removedInput = document.getElementById("player2Input")
@@ -43,7 +43,7 @@ function player3() {
 function player4() {
     player3Name = document.getElementById("player3Input").value
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     console.log(`Welkom ${player3Name}`)
     const removedInput = document.getElementById("player3Input")
@@ -57,7 +57,7 @@ function player4() {
 function player5() {
     player4Name = document.getElementById("player4Input").value
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     console.log(`Welkom ${player4Name}`)
     const removedInput = document.getElementById("player4Input")
@@ -71,7 +71,7 @@ function player5() {
 function deMol() {
     player5Name = document.getElementById("player5Input").value
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     console.log(`Welkom ${player5Name}`)
     const removedInput = document.getElementById("player5Input")
@@ -85,7 +85,7 @@ function deMol() {
 function startGame() {
     deMolName = document.getElementById("deMolInput")
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     const removedInput = document.getElementById("deMolInput")
     body.removeChild(removedInput)
@@ -95,15 +95,81 @@ function startGame() {
 }
 function opdracht1() {
     const body = document.querySelector("body")
-    const p = document.querySelector("p")
+    const p = document.getElementById("mainP")
     const usedButton = document.querySelector("button")
     const codeInput = document.createElement("input")
     body.appendChild(codeInput)
     codeInput.id = "guessedCode"
-    p.innerHTML = `Tijd voor jullie eerste opdracht. Jullie hebben een kaart gekregen met 5 locaties op. Op deze locaties vinden jullie een cijfer of een letter die jullie nodig hebben voor de code. Elke speler mag maar één cijfer nemen, als je een cijfer:letter hebt kom je terug naar huis om te wachten op de rest. Maar pas op! Er is een zoeker in het spel, als de zoeker iemand vindt moet die speler terug naar huis gaan en daar 2 minuten wachten. Open hiervoor allemaal de Gotcha app a.u.b Voor deze opdracht krijgen jullie x minuten. Start!`
-    usedButton.innerHTML = 'verzend code.'   
+    p.innerHTML = `Ronde 1, kraak de code!`
+    usedButton.innerHTML = 'verzend code'   
     usedButton.onclick = checkCode
 }
 function checkCode() {
-    LOREM
+    const enteredCode = document.getElementById("guessedCode").value
+    const correctCode = "p5x9t45g"
+    if (enteredCode === correctCode) {
+        alert ("Proficiat, jullie hebben de juiste code gevonden!")
+        const body = document.querySelector("body")
+        const p = document.getElementById("mainP")
+        const usedButton = document.querySelector("button")
+        const codeInput = document.getElementById("guessedCode")
+        body.removeChild(codeInput)
+        body.removeChild(usedButton)
+        p.innerHTML = "Ronde 2, ken je vrienden!"
+        const question1Title = document.createElement("h3")
+        const question1Question = document.createElement("p")
+        const question1Input = document.createElement("input")
+        const question2Title = document.createElement("h3")
+        const question2Input = document.createElement("input")
+        const question2Question = document.createElement("p")
+        const question3Title = document.createElement("h3")
+        const question3Input = document.createElement("input")
+        const question3Question = document.createElement("p")
+        const question4Title = document.createElement("h3")
+        const question4Input = document.createElement("input")
+        const question4Question = document.createElement("p")
+        const newButton = document.createElement("button")
+        body.appendChild(question1Title)
+        body.appendChild(question1Question)
+        body.appendChild(question1Input)
+        body.appendChild(question2Title)
+        body.appendChild(question2Question)
+        body.appendChild(question2Input)
+        body.appendChild(question3Title)
+        body.appendChild(question3Question)
+        body.appendChild(question3Input)
+        body.appendChild(question4Title)
+        body.appendChild(question4Question)
+        body.appendChild(question4Input)
+        body.appendChild(newButton)
+        question1Title.id = "title1"
+        question1Input.id = "input1"
+        question1Question.id = "question1"
+        question2Title.id = "title2"
+        question2Input.id = "input2"
+        question2Question.id = "question2"
+        question3Title.id = "title3"
+        question3Input.id = "input3"
+        question3Question.id = "question3"
+        question4Title.id = "title4"
+        question4Input.id = "input4"
+        question4Question.id = "question4"
+        question1Title.innerHTML = "Vraag1"
+        question1Question.innerHTML = "Van wie is dit het lievelingsliedje?"
+        question2Title.innerHTML = "Vraag2"
+        question2Question.innerHTML = "Bij wie past deze eigenschap het best?"
+        question3Title.innerHTML = "Vraag3"
+        question3Question.innerHTML = "Van wie is dit het lievelingsliedje?"
+        question4Title.innerHTML = "Vraag4"
+        question4Question.innerHTML = "Van wie is dit het lievelingsliedje?"
+        newButton.innerHTML = "Verzend antwoorden"
+        newButton.onclick = checkAnswers
+    } else {
+        alert ("Sorry deze code is niet juist, gelieve opnieuw te proberen.")
+        const usedButton = document.querySelector("button")
+        usedButton.onclick = checkCode
+    }
+}
+function checkAnswers () {
+    f
 }
